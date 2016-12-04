404433614d024ba3bd8e69b0bb421ee92d2da022
Rardd1997 <therardd@gmail.com>
2016-12-04 02:38:20 +0200

    Update FileManager.cs
    Create handles for NewFile, SaveFile and SaveAsFile

--- VakunTranslatorVol2/FileManager.cs
+++ VakunTranslatorVol2/FileManager.cs
@@ -24,5 +24,70 @@ namespace VakunTranslatorVol2
                 return false;
             }
         }
+        public bool CreatFileDialog()
+        {
+            string defaultExt = ".txt";
+
+            var dialog = new SaveFileDialog();
+            dialog.DefaultExt = defaultExt;
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = dialog.FileName;
+                dialog.OpenFile().Close();
+                return true;
+            }
+            else
+            {
+                fileName = null;
+                return false;
+            }
+        }
+        public bool SaveFileDialog(string _text)
+        {
+            if (File.Exists(fileName))
+            {
+                try
+                {
+                    File.WriteAllText(fileName, _text);
+                    return true;
+                }
+                catch (IOException)
+                {
+                    return false;
+                }
+            }
+            else
+            {
+                MessageBox.Show("Your file does not exist!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                fileName = null;
+                return false;
+            }
+        }
+        public bool SaveAsFileDialog(string _text)
+        {
+            var dialog = new SaveFileDialog();
+
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    fileName = dialog.FileName;
+                    dialog.OpenFile().Close();
+                    File.WriteAllText(fileName, _text);
+                    return true;
+                }
+                catch (IOException)
+                {
+                    MessageBox.Show("We encountered an error when saving a file!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return false;
+                }
+            }
+            else
+            {
+                fileName = null;
+                return false;
+            }
+        }
     }
 }
